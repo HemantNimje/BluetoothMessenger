a75739d4322b52c4c74b636f3d77de5597fd96fb
Hemant <nimje.hemant@gmail.com>
2017-05-05 13:54:15 -0700

    Add new files

---
+++ app/src/main/java/edu/csulb/android/bluetoothmessenger/DeviceListActivity.java
@@ -0,0 +1,8 @@
+package edu.csulb.android.bluetoothmessenger;
+
+/**
+ * Created by Hemant on 5/5/2017.
+ */
+
+public class DeviceListActivity {
+}
---
+++ app/src/main/java/edu/csulb/android/bluetoothmessenger/ProfileActivity.java
@@ -0,0 +1,8 @@
+package edu.csulb.android.bluetoothmessenger;
+
+/**
+ * Created by Hemant on 4/7/2017.
+ */
+
+public class ProfileActivity {
+}
