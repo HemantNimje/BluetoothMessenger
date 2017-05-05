d9400cf17ee5f2d564e06baab059824cbf8e6b70
Hemant <nimje.hemant@gmail.com>
2017-05-05 14:53:17 -0700

    Add profile activity to the menu options on main activity

--- app/src/main/java/edu/csulb/android/bluetoothmessenger/ProfileActivity.java
+++ app/src/main/java/edu/csulb/android/bluetoothmessenger/ProfileActivity.java
@@ -1,8 +1,19 @@
 package edu.csulb.android.bluetoothmessenger;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Hemant on 4/7/2017.
  */
 
-public class ProfileActivity {
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
