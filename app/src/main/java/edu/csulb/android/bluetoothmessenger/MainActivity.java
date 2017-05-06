f36091841b5db4d5db0d58aefa67fbc7ea39bb4b
Andrew Tran <andrewalvintran@gmail.com>
2017-05-06 14:44:20 -0700

    Setup ListView to display chat history

--- app/src/main/java/edu/csulb/android/bluetoothmessenger/MainActivity.java
+++ app/src/main/java/edu/csulb/android/bluetoothmessenger/MainActivity.java
@@ -6,17 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView chatHistoryListView;
-    private String[] historyHolderText = {"User 1", "User 2", "User 3", "User 4", "User 5", "User 6", "User 7"};
-
+    private ArrayList<String> previousChatNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,6 @@ public class MainActivity extends AppCompatActivity {
 
         chatHistoryListView = (ListView) findViewById(R.id.list_chat_history);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, historyHolderText);
-        chatHistoryListView.setAdapter(adapter);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +39,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(newChatIntent);
             }
         });
+
+
+        // Start chat history and allow user to start connection
+        // Currently ChatHistory is a stub to test that messages are in the
+        // correct order
+        chatHistoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent intent = new Intent(MainActivity.this, ChatHistory.class);
+                String userInfo = chatHistoryListView.getItemAtPosition(position).toString();
+                String splitLines[] = userInfo.split("[\\r?\\n]+");
+                String userName = splitLines[0];
+                String macAddress = splitLines[1];
+                intent.putExtra("MAC-ADDRESS", macAddress);
+                intent.putExtra("DEVICE-NAME", userName);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    // Update chat history when you return
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Messages db = new Messages(getApplicationContext());
+        previousChatNames = (ArrayList<String>) db.getPreviousChatNames();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, previousChatNames);
+        chatHistoryListView.setAdapter(adapter);
     }
 
     @Override
@@ -53,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -67,4 +99,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
