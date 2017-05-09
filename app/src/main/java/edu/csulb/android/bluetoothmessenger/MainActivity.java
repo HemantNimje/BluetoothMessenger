a771ec5c463e710e483ccd4326bdba6df7346f1b
Hemant <nimje.hemant@gmail.com>
2017-05-09 01:42:14 -0700

    Make device discoverable from group chat activity page.

--- app/src/main/java/edu/csulb/android/bluetoothmessenger/MainActivity.java
+++ app/src/main/java/edu/csulb/android/bluetoothmessenger/MainActivity.java
@@ -110,9 +110,19 @@ public class MainActivity extends AppCompatActivity {
                 Intent groupChatIntent = new Intent(getApplicationContext(),GroupChatDeviceListActivity.class);
                 startActivity(groupChatIntent);
                 break;
+            case R.id.group_chat_make_discoverable:
+                ensureDiscoverable();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
 }
